20cfa67b30a2e4c37533ba07d9d1cbb9f8562895
sandipchitale <sandipchitale@e3db8ebe-c94a-11dd-8ca8-e3ff79f713b6>
2008-12-13 19:23:14 +0000

    Initial check in.

---
+++ SmartSemicolon/src/smartsemicolon/AbstractAddSemicolonCommand.java
@@ -0,0 +1,74 @@
+package smartsemicolon;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
+
+public abstract class AbstractAddSemicolonCommand implements IHandler {
+
+	public void addHandlerListener(IHandlerListener handlerListener) {
+
+	}
+
+	public void dispose() {
+
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow();
+		if (activeWorkbenchWindow == null) {
+			return null;
+		}
+		IEditorPart activeEditor = activeWorkbenchWindow.getActivePage()
+				.getActiveEditor();
+		if (activeEditor instanceof AbstractDecoratedTextEditor) {
+			AbstractDecoratedTextEditor editor = (AbstractDecoratedTextEditor) activeEditor;
+			Object adapter = (Control) editor.getAdapter(Control.class);
+			if (adapter instanceof Control) {
+				Control control = (Control) adapter;
+				if (control instanceof StyledText) {
+					StyledText styledText = (StyledText) control;
+					doAddSemicolon(styledText);
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean isEnabled() {
+		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow();
+		if (activeWorkbenchWindow == null) {
+			return false;
+		}
+		IEditorPart activeEditor = activeWorkbenchWindow.getActivePage()
+				.getActiveEditor();
+		if (activeEditor instanceof AbstractDecoratedTextEditor) {
+			AbstractDecoratedTextEditor editor = (AbstractDecoratedTextEditor) activeEditor;
+			Object adapter = (Control) editor.getAdapter(Control.class);
+			if (adapter instanceof Control) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isHandled() {
+		return isEnabled();
+	}
+
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+
+	}
+	
+	protected abstract void doAddSemicolon(StyledText styledText);
+
+}
